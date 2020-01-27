3a408a1b908c71824066d6777a6a97b8db83cf66
chingaloEric <echingalo@gmail.com>
2020-01-27 09:16:08 +0300

    feat: 🎸 added global prefix as 'api'

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './core/filters/http-exception.filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // to set global filter
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
